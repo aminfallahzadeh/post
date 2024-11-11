11f04e2859c1a5b687d0f2d4563909993bdf00cc
amin-dev-tech <amin.dev.tech@gmail.com>
2024-11-11 19:55:23 +0300

    fix download pdf

--- components/DownloadPDF.jsx
+++ components/DownloadPDF.jsx
@@ -2,40 +2,15 @@
 import { Pressable, Text, Image } from "react-native";
 
 // EXPO
-import * as FileSystem from "expo-file-system";
+import { router } from "expo-router";
 
 // ASSETS
 import images from "@/constants/images";
 
 export const DownloadPDF = ({ item }) => {
-  // HANDLE PDF DOWNLOAD
-  // const donwload = async () => {
-  //   const pdfUrl = item?.result?.certificateUrl;
-  //   const fileUri = `${FileSystem.documentDirectory}${
-  //     item.postcode || "download"
-  //   }.pdf`;
-
-  //   try {
-  //     // Download the PDF file
-  //     await FileSystem.downloadAsync(pdfUrl, fileUri);
-  //   } catch (error) {
-  //     alert("Error", "دانلود ناموفق");
-  //     console.error("Download error:", error);
-  //   }
-  // };
-
   const download = async () => {
     const url = item?.result?.certificateUrl;
-    const fileName = item?.postcode || "download";
-    const fileUri = FileSystem.documentDirectory + fileName;
-
-    try {
-      const result = await FileSystem.downloadAsync(url, fileUri);
-      console.log("File downloaded to:", result.uri);
-      alert("Download complete! File saved to app storage.");
-    } catch (error) {
-      console.log("Error downloading file:", error);
-    }
+    router.push(url);
   };
 
   return (
